c29248525c5442b638634c97d6ee3bf5d9285c67
TheHulkSmash <50568843+TheHulkSmash@users.noreply.github.com>
2019-10-02 04:34:08 -0700

    Connecting to database

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
+var express = require('express');
 
-var mysqlconnection = mysql.createConnection({
-    host: 'localhost',
-    user: '', 
-    password: ' ',
-    database: ''
-})
-
-mysqlconnection.connect((err) =>{
-    if(!err)
-        console.log('Database is connected.');
-    else
-        console.log('Database is not connected \n Error : ' + JSON.stringify(err, undefined, 2));
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'reinard',
+    database : 'timetable'
 });
 
-app.listen(3000, ()=> console.log('Express server is running at port 3000'));
+connection.connect(function(err) {
+    // in case of error
+    if(!!err){
+        console.log('error');
+    }
+    else{
+         console.log('connected');
+    }
+});
 
-app.get('/employees', (res,req) => {
-    mysqlconnection.query('SELECT * FROM Employees', (err, rows, fields)=>{
+$query = 'SELECT * from day ';
+
+connection.query($query, function(err, rows, fields) {
+    if(err){
+        console.log("An error ocurred performing the query.");
+        return;
+    }
+
+    console.log("Query succesfully executed: ", rows);
+});
+//app.listen(3000, ()=> console.log('Express server is running at port 3000'));
+
+/*app.get('/user', (res,req) => {
+    mysqlconnection.query('SELECT * FROM user', (err, rows, fields)=>{
         if(!err)
             console.log(rows);
         else
             console.log(err);
     })
-});
+});*/
