47a6966237b2220c20fbc5a2e57ac92b650858f8
28552296 [18] <50983790+28552296@users.noreply.github.com>
2019-10-22 15:57:44 +0200

    creating variables for the passport.js file

---
+++ passport.js
@@ -0,0 +1,9 @@
+var LocalStrategy = require("passport-local").Strategy;
+
+var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+var dbconfig = require('./database');
+var connection = mysql.createConnection(dbconfig.connection);
+
+require('./config/passport')(passport);
+import "../config/passport";
