f6d38a4cca0d7b0bccee90e3e89498c79fdc3cd6
28552296 [18] <50983790+28552296@users.noreply.github.com>
2019-10-22 16:11:47 +0200

    inserting data into the database via the sign up page

--- passport.js
+++ passport.js
@@ -7,3 +7,37 @@ var connection = mysql.createConnection(dbconfig.connection);
 
 require('./config/passport')(passport);
 import "../config/passport";
+
+passport.use(
+    'local-signup',
+    new LocalStrategy({
+     usernameField : 'username',
+     passwordField: 'password',
+     passReqToCallback: true
+    },
+    function(req, username, password, done){
+     connection.query("SELECT * FROM user WHERE username = ? ", 
+     [username], function(err, rows){
+      if(err)
+       return done(err);
+      if(rows.length){
+       return done(null, false, req.flash('signupMessage', 'That is already taken'));
+      }else{
+       var newUserMysql = {
+        username: username,
+        password:password
+        //password: bcrypt.hashSync(password, null, null)
+       };
+  
+       var insertQuery = "INSERT INTO user (username, password) values (?, ?)";
+  
+       connection.query(insertQuery, [newUserMysql.username, newUserMysql.password],
+        function(err, rows){
+         newUserMysql.username = rows.username;
+  
+         return done(null, newUserMysql);
+        });
+      }
+     });
+    })
+   );
